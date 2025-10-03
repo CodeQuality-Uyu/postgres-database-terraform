78bdb3072e82d9c4634ef0193bf97ee27604a373
Daniel Acevedo <daniel18acevedo@gmail.com>
2025-10-03 10:34:27 -0300

    Update data-remote.tf

--- data-remote.tf
+++ data-remote.tf
@@ -6,15 +6,6 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
-# (opcional) si querés SG de tasks desde el cluster/servicio
-data "terraform_remote_state" "ecs" {
-  backend = "remote"
-  config = {
-    organization = var.remote_state_org
-    workspaces = { name = var.remote_state_cluster_ws } # o servicio
-  }
-}
-
 data "terraform_remote_state" "bastion" {
   backend = "remote"
   config = {
